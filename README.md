4f56a3cca37af09e7a2161b5cb6611acb506b461
Kos1la <kos1la1998@gmail.com>
2024-09-03 15:35:36 +0300

    step-8

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 
 [![Maintainability](https://api.codeclimate.com/v1/badges/003d78b674feebd598fb/maintainability)](https://codeclimate.com/github/Kos1la/backend-project-44/maintainability)
 
-[![asciicast](https://asciinema.org/a/Y7Q3AZh3PihgC58cM0HI67BMh.svg)](https://asciinema.org/a/Y7Q3AZh3PihgC58cM0HI67BMh)
+[![asciicast](https://asciinema.org/a/BJsjNI2Henq3vkzvDCq2wcgZ8.svg)](https://asciinema.org/a/BJsjNI2Henq3vkzvDCq2wcgZ8)
---
+++ src/games/progression.js
@@ -0,0 +1,36 @@
+import { question } from 'readline-sync';
+import { runGame, randomNum } from '../index.js';
+
+const description = 'What number is missing in the progression?';
+
+const generateArray = (start, step) => {
+  const array = [];
+
+  array.push(start);
+
+  for (let i = 0; i < 9; i++) {
+    array.push(array[i] + step);
+  }
+  return array;
+};
+
+const generateQuestion = () => {
+  const start = randomNum(1, 10);
+  const step = randomNum(2, 10);
+
+  const array = generateArray(start, step);
+
+  const hiddenNum = randomNum(0, 9); // array.length -1
+
+  const correctAnswer = array[hiddenNum];
+  array[hiddenNum] = '..';
+
+  return {
+    question: array.join(' '),
+    correctAnswer: correctAnswer.toString(),
+  };
+};
+
+const brainProgression = () => runGame(description, generateQuestion);
+
+export default brainProgression;
